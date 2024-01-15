16f8f7ce4fd676086d2860703ff343987cf3c971
BeautifulAngel246 <favouronuche@gmail.com>
2024-01-15 09:58:27 -0800

    testmodels

---
+++ 0x0C-python-almost_a_circle/tests/12-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+sys.path.append("/alx-higher_level_programming/0x0C-python-almost_a_circle")
+
+""" 12-main """
+from models.rectangle import Rectangle
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(10, 2, 1, 9)
+    print(r1)
+    r1_dictionary = r1.to_dictionary()
+    print(r1_dictionary)
+    print(type(r1_dictionary))
+
+    r2 = Rectangle(1, 1)
+    print(r2)
+    r2.update(**r1_dictionary)
+    print(r2)
+    print(r1 == r2)
---
+++ 0x0C-python-almost_a_circle/tests/7-main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Doc """
+
+import sys
+sys.path.append("/alx-higher_level_programming/0x0C-python-almost_a_circle")
+
+from models.rectangle import Rectangle
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(10, 10, 10, 10)
+    print(r1)
+
+    r1.update(89)
+    print(r1)
+
+    r1.update(89, 2)
+    print(r1)
+
+    r1.update(89, 2, 3)
+    print(r1)
+
+    r1.update(89, 2, 3, 4)
+    print(r1)
+
+    r1.update(89, 2, 3, 4, 5)
+    print(r1)
