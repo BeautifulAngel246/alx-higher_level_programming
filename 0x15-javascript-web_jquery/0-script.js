43447eeafc9e58bcf8862d58eac107aebd30fe6c
BeautifulAngel246 <favouronuche@gmail.com>
2024-05-02 14:34:46 -0700

    firstscript

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,1 +1,6 @@
-document.querySelector('header').style.color = '#FF0000';
+#!/usr/bin/node
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
+
